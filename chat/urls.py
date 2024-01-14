8c28ea2e7ef924ee0e8b506c1fb1b6f2cf2a54ca
Weshy Joe <josephwaweru96@gmail.com>
2024-01-14 12:14:00 +0300

    templates + views

--- chat/urls.py
+++ chat/urls.py
@@ -4,4 +4,10 @@ from . import views
 
 urlpatterns = [ 
     path('', views.index, name='index'),
+    path('meet/<str:room_name>/', views.room, name='chat-room'),
+    path("base/", views.notice, name='notice'),
+    path("talk/", views.talk, name='talk'),
+    path("group/<str:group_name>/",views.group, name='group'),
+    path("signup/", views.RegistrationView.as_view(), name='signup'),
+    path("login/", views.SigninView.as_view(), name='login'),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,61 @@
 from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.views import LoginView
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+
+from .models import Room, Message
 
 # Create your views here.
+def index(request):
+    room = Room.objects.all()
+    
+    return render(request, 'index.html', {'room': room})
+
+
+def room(request, room_name):
+    """create room or retrieve it"""
+    chat_room, created =  Room.objects.get(name=room_name)
+    
+    return render(request, 'room.html', {'room': chat_room})
+
+
+@login_required
+def group(request, group_name):
+    group = Room.objects.get(id=group_name)
+    messages = group.message_set.all()
+
+    context = {
+        "messages": messages, 
+        "room_name": group_name,
+        "name": group
+    }
+
+    return render(request, 'group-chat.html', context)
+
+
+def notice(request):
+    return render(request, "base.html")
+
+
+def talk(request):
+    messages = Message.objects.all()
+    groups = Room.objects.all()
+
+    context = {
+        'messages': messages, 
+        'groups': groups}
+    
+    return render(request, 'talk.html', context)
+
+
+class RegistrationView(CreateView):
+    template_name = 'signup.html'
+    model = get_user_model()
+    success_url = reverse_lazy("login")
+
+
+class SigninView(LoginView):
+    next_page = '/chat/talk/'
+    template_name = 'signin.html'
